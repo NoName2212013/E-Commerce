c3e1ee49b42e0794731f3b6dfbb24b6221598f85
NoName2212013 <rajdeepbarman733@gmail.com>
2024-08-13 00:42:37 +0530

    Minor Changes

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ownerModel = require('../models/owner-model');
 
-if(process.env.NODE_ENV === "development"){
     router.post("/create",async function (req,res){
         let owners = await ownerModel.find();
         if(owners.length>0) {
@@ -20,7 +19,6 @@ if(process.env.NODE_ENV === "development"){
         });
         res.status(201).send(createdOwner);
     });
-}
 
 router.get("/admin",function(req,res){
     let success = req.flash("success");
