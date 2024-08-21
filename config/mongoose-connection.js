4a96ba97e033c33e7b0d1fa11afc9b4356a089ef
NoName2212013 <rajdeepbarman733@gmail.com>
2024-08-21 18:40:50 +0530

    Database Changed to Online type

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const dbgr = require('debug')('development:mongoose');
 const config = require("config");
 
-mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
+mongoose.connect(config.get("MONGODB_URI"))
 .then(()=>{
     dbgr("connected");
+    console.log("Database Connected...");
 })
 .catch((err)=>{
     dbgr(err);
